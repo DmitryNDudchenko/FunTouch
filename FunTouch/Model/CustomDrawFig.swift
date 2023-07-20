2ea1d2697f108955482b9f18e2a65a9629e72406
Dmitry Dudchenko <dmitry.dudchenko@ronis.info>
2023-07-20 22:07:26 +0300

    no message

--- FunTouch/Model/CustomDrawFig.swift
+++ FunTouch/Model/CustomDrawFig.swift
@@ -6,15 +6,33 @@
 //
 
 import UIKit
-
+@IBDesignable
 class CustomDrawFig: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+   @IBInspectable var startColor: UIColor = UIColor.red
+   @IBInspectable var endColor: UIColor = UIColor.yellow
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: [UIRectCorner.topLeft, UIRectCorner.topRight], cornerRadii: CGSize(width: 35, height: 35))
+        path.addClip()
+        
+        guard let context = UIGraphicsGetCurrentContext() else {return}
+        
+        let colors = [startColor.cgColor, endColor.cgColor]
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let colorLocation: [CGFloat] = [0.0, 1.0]
+        guard let gradient = CGGradient(
+            colorsSpace: colorSpace,
+            colors: colors as CFArray,
+            locations: colorLocation
+        ) else {
+            return
+        }
+        
+        let startPoint = CGPoint.zero
+        let endPoint = CGPoint(x: 0, y: self.bounds.height)
+        
+        context.drawLinearGradient(gradient, start: startPoint, end: endPoint, options: CGGradientDrawingOptions.drawsAfterEndLocation)
     }
-    */
 
 }
--- FunTouch/Snippet.swift
+++ FunTouch/Snippet.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+// Учбовий приклад UserDefaults
+func userDefaults() {
+    
+    var dict = [
+        "name" : "Dmutro",
+        "Language" : "Swift"
+    ]
+    
+    let name = "Dmutro"
+    
+    UserDefaults.standard.set(name, forKey: "name") // Встановлення змінної
+    UserDefaults.standard.removeObject(forKey: "name") // Видалення значення
+    
+    guard let name = UserDefaults.standard.string(forKey: "name") else { // безпечний доступ і повернення значення
+        return
+    }
+    
+    let defaults = UserDefaults.standard // Скорочення синтаксису
+    
+    let newName = defaults.string(forKey: "name") ?? "Unknown"
+    
+}
